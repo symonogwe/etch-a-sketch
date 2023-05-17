7a5bb9f7086acef45289df737c0a320a1e72a0b9
symonogwe <symonogwe@gmail.com>
2023-05-17 20:27:32 +0300

    Create etch-div variable and append 32 grid-divs using a for loop, appended 32 grid divs within etch-div container.

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-let r = document.querySelector(".container");
-console.log(r);
+
+// html variables
+let etchContainer = document.getElementById("etch");
+
+
+// for loop to create grid div 32 times
+for (let i = 0; i < 32; i++) {
+    let gridDiv = document.createElement("div");
+    gridDiv.classList.add("grid-div");
+    etchContainer.appendChild(gridDiv);
+}
+
+console.log(etchContainer);
