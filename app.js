8ab5b477b61e29156f818423b2b8ee5e8929aeb8
symonogwe <symonogwe@gmail.com>
2023-05-18 21:10:05 +0300

    Add event listener to button

--- app.js
+++ app.js
@@ -2,15 +2,36 @@
 // html variables
 let etchContainer = document.getElementById("etch");
 
-//prompt for grid dimension
 
-
-// for loop to create grid div of 16 by 16
-for (let i = 0; i < (32 * 32); i++) {
-    let gridDiv = document.createElement("div");
-    gridDiv.classList.add("grid-div");
-    etchContainer.appendChild(gridDiv);
+function setDimension () {
+    let input = Number(prompt("16 or 32 or 64"));
+    if (input === 16 || input === 32 || input === 64) {
+        for (let i = 0; i < (input * input); i++) {
+            let gridDiv = document.createElement("div");
+            gridDiv.classList.add("grid-div");
+            gridDiv.style.width = "calc(322 / input)";
+            gridDiv.style.height = "calc(322 / input)";
+            gridDiv.style.backgroundColor = "white";
+            gridDiv.style.border = "1px solid black";
+            etchContainer.appendChild(gridDiv);
+        }
+    } else {
+        alert("enter valid")
+    }
 }
+
+//add Event Listener to button
+let button = document.getElementById("dimension-prompt");
+button.addEventListener("click", setDimension);
+
+
+
+// // for loop to create grid div of 16 by 16
+// for (let i = 0; i < (64 * 64); i++) {
+//     let gridDiv = document.createElement("div");
+//     gridDiv.classList.add("grid-div");
+//     etchContainer.appendChild(gridDiv);
+// }
 
 // add Event Listener to all div grids
 const allDivGrid = document.querySelectorAll(".grid-div");
